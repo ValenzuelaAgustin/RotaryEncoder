c2ebb6c472ab5bc27b3f00c4d76e5b5586acffdc
Agustin Valenzuela <103297903+ValenzuelaAgustin@users.noreply.github.com>
2023-07-19 12:59:33 -0300

    Update RotaryEncoder.hpp

--- RotaryEncoder.hpp
+++ RotaryEncoder.hpp
@@ -2,19 +2,14 @@
 #define ROTARYENCODER_HPP
 #include <Arduino.h>
 
-#ifndef byte
-#define byte unsigned char
-#endif
-
 class RotaryEncoder
 {
 public:
     RotaryEncoder(byte A_pin, byte B_pin);
     void begin(void(*ISR)());
-
     void checkPosition();
 
-    volatile int position = 0;
+    volatile int position;
 
 private:
     enum {A = 0, B = 1};
