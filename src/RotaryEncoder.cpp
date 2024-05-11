98483080fab248b5ceeeac794f9378d5656fbfe3
Agustin Valenzuela <103297903+ValenzuelaAgustin@users.noreply.github.com>
2024-05-11 18:01:02 -0300

    .

--- src/RotaryEncoder.cpp
+++ src/RotaryEncoder.cpp
@@ -17,8 +17,8 @@ void RotaryEncoder::begin(void(*ISR)())
 void RotaryEncoder::checkPosition()
 {
     ab = (bool)digitalRead(pin[A]) * 0x02 + (bool)digitalRead(pin[B]) * 0x01;
-    if ((AB & 0x0c) == ab) AB >>= 2;
-    if ((AB & 0x03) == ab) return;
+    if ((AB & 0x0c) == ab) AB >>= 2; // Checks if the encoder has stepped back.
+    if ((AB & 0x03) == ab) return;   // Checks if the encoder is in the same step as before.
     AB <<= 2;
     AB |= ab;
     position += (AB == CW) - (AB == CCW);
